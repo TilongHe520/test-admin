b5d3af55a8af130c377d1ba61b93d16631e20c06
TilongHe520 <463673840@qq.com>
2021-05-16 22:29:52 +0800

    整合log4j2日志

--- test-admin-back/src/main/java/com/admin/controller/LoginController.java
+++ test-admin-back/src/main/java/com/admin/controller/LoginController.java
@@ -2,18 +2,24 @@ package com.admin.controller;
 
 import com.admin.common.CommonResult;
 import com.admin.pojo.UserInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class LoginController {
 
     @RequestMapping(value = "/api/login", method = RequestMethod.POST)
     @CrossOrigin
     public CommonResult login(@RequestBody UserInfo user) {
-        System.out.println(user.toString());
-        if (user.getUserName().equals("admin") && user.getPassword().equals("123456"))
-            return CommonResult.success(user,"登录成功");
-        else
+
+        if (user.getUserName().equals("admin") && user.getPassword().equals("123456")) {
+            log.info(user.toString());
+            return CommonResult.success(user, "登录成功");
+        } else{
+            log.error(user.toString());
             return CommonResult.validateFailed("用户名或密码错误");
+        }
+
     }
 }
